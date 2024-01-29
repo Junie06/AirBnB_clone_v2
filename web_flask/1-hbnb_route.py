f225aad0a10aa09a6e2c3f9d77f0d1a003fa2229
Isoken June Ibizugbe <isokenjune@gmail.com>
2024-01-29 03:54:57 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''starts a Flask web application'''
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -17,5 +18,5 @@ def hbnb():
     return 'HBNB'
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
